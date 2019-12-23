f2ce5947f70eb338becf09249a1d2a27ac53b211
devjosegomez <devjosegomez@gmail.com>
2019-12-22 20:53:00 -0600

    Added Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="main-nav">
+        <ul>
+            <li>
+                <router-link to="/">Home</router-link>
+                <router-link :to="{name: 'About'}">About</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style >
+
+ul{
+    text-decoration: none;
+    padding: 0px;
+}
+
+a{
+    color: darkseagreen;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
 
+
 Vue.use(Router)
 
 export default new Router({
